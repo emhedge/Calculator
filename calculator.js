ae833db5e0fec815d40fdac1fce7fbdb5d00ae14
emhedge <122066271+emhedge@users.noreply.github.com>
2025-12-04 16:44:07 +0800

    Added init variables and calculate() function

---
+++ calculator.js
@@ -0,0 +1,34 @@
+// basic calculations
+const add = (a, b) => a + b;
+
+const subtract = (a, b) => a - b;
+
+// const sum = function(arr) {return arr.reduce(((acc, obj) => acc + obj), 0)}
+
+const multiply = function(arr) {return arr.reduce(((first, second) => first * second), 1)}
+
+// const power = (a, b) => a ** b;
+
+// function factorial(a) {
+// // prevent negatives
+//   if (a < 0) return "ERROR";
+// // work with 0 and 1
+//   if (a === 0 || a === 1) return 1;
+// // work with numbers > 1
+//   if (a > 1) {
+//     let result = 1;
+//     for (let i = a; i > 1; i--) {
+//     result *= i
+//     } 
+//   return result
+//   }
+// }
+
+let num1 = 0;
+let num2 = 0;
+let operator = 0;
+
+function operate(num1, operator, num2) {
+    return true;
+}
+
