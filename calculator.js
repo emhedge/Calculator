7ea5383b36aa9643cd726f612d2112bb61c7b981
emhedge <122066271+emhedge@users.noreply.github.com>
2025-12-13 20:44:34 +0800

    Added button variables, basic event listeners
    Created button event listeners to directly display
    button pressed in display.

--- calculator.js
+++ calculator.js
@@ -4,19 +4,6 @@ const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
-
-// button functions
-const numZero = document.querySelector("#0");
-const numOne = document.querySelector("#1");
-const numTwo = document.querySelector("#2");
-const numThree = document.querySelector("#3");
-const numFour = document.querySelector("#4");
-const numFive = document.querySelector("#5");
-const numSix = document.querySelector("#6");
-const numSeven = document.querySelector("#7");
-const numEight = document.querySelector("#8");
-const numNine = document.querySelector("#9");
-
 // const sum = function(arr) {return arr.reduce(((acc, obj) => acc + obj), 0)}
 
 // const power = (a, b) => a ** b;
@@ -36,10 +23,12 @@ const numNine = document.querySelector("#9");
 //   }
 // }
 
+// init declaration of numbers and operator
 let num1 = 0;
 let num2 = 0;
 let operator = "";
 
+// operate function
 function operate(num1, operator, num2) {
     if (operator == "+") {
         return add(num1, num2)
@@ -53,5 +42,74 @@ function operate(num1, operator, num2) {
     
     
 }
+
+// button variables
+const numZero = document.querySelector("#numZero");
+const numOne = document.querySelector("#numOne");
+const numTwo = document.querySelector("#numTwo");
+const numThree = document.querySelector("#numThree");
+const numFour = document.querySelector("#numFour");
+const numFive = document.querySelector("#numFive");
+const numSix = document.querySelector("#numSix");
+const numSeven = document.querySelector("#numSeven");
+const numEight = document.querySelector("#numEight");
+const numNine = document.querySelector("#numNine");
+
+
+
+
+// update display function
+const display = document.querySelector("div#display");
+const displayText = document.createElement('p');
+displayText.textContent = "";
+
+display.appendChild(displayText);
+// function updateDisplay() {
+//     displayText.textContent = 
+// }
+
+// button event handlers for various numbers/operators
+const buttons = document.querySelector("#buttons");
+buttons.addEventListener("click", (event) => {
+    let target = event.target;
+
+    switch(target.id) {
+        case "numZero":
+            displayText.textContent = "0"
+            break;
+        case "numOne":
+            displayText.textContent = "1"
+            break;
+        case "numTwo":
+            displayText.textContent = "2"
+            break;
+        case "numThree":
+            displayText.textContent = "3"
+            break;
+        case "numFour":
+            displayText.textContent = "4"
+            break;
+        case "numFive":
+            displayText.textContent = "5"
+            break;
+        case "numSix":
+            displayText.textContent = "6"
+            break;
+        case "numSeven":
+            displayText.textContent = "7"
+            break;
+        case "numEight":
+            displayText.textContent = "8"
+            break;
+        case "numNine":
+            displayText.textContent = "9"
+            break;
+    }
+})
+
+
+
+
+
 
 console.log(operate(10, "/", 2))
