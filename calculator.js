b7b320a1f5632212e359d5890b42d82acd07cf99
emhedge <122066271+emhedge@users.noreply.github.com>
2025-12-13 20:13:42 +0800

    Added operate(), buttons, basic CSS with flex
    Filled out operate() function, added basic buttons
    along with querySelector for each, implemented
    basic CSS to align the buttons on the page for
    js debugging.

--- calculator.js
+++ calculator.js
@@ -1,11 +1,23 @@
 // basic calculations
 const add = (a, b) => a + b;
-
 const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+
+// button functions
+const numZero = document.querySelector("#0");
+const numOne = document.querySelector("#1");
+const numTwo = document.querySelector("#2");
+const numThree = document.querySelector("#3");
+const numFour = document.querySelector("#4");
+const numFive = document.querySelector("#5");
+const numSix = document.querySelector("#6");
+const numSeven = document.querySelector("#7");
+const numEight = document.querySelector("#8");
+const numNine = document.querySelector("#9");
 
 // const sum = function(arr) {return arr.reduce(((acc, obj) => acc + obj), 0)}
-
-const multiply = function(arr) {return arr.reduce(((first, second) => first * second), 1)}
 
 // const power = (a, b) => a ** b;
 
@@ -26,9 +38,20 @@ const multiply = function(arr) {return arr.reduce(((first, second) => first * se
 
 let num1 = 0;
 let num2 = 0;
-let operator = 0;
+let operator = "";
 
 function operate(num1, operator, num2) {
-    return true;
+    if (operator == "+") {
+        return add(num1, num2)
+    } else if (operator === "-") {
+        return subtract(num1, num2)
+    } else if (operator === "*") {
+        return multiply(num1, num2)
+    } else if (operator === "/") {
+        return divide(num1, num2)
+    } else return "ERROR";
+    
+    
 }
 
+console.log(operate(10, "/", 2))
